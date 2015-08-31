4e7d68eb45fe7a9239b824ba535c494e103ddee8
Jason Luck <jason.luck@gmail.com>
2015-08-31 19:06:48 -0400

    Reverting change to download zip

--- scripts/sr-dev-interview.sh
+++ scripts/sr-dev-interview.sh
@@ -7,15 +7,10 @@ su - vagrant
 #Add anything here that is needed specifically for the Sr. Java Developer Interview
 
 #Download the POM file and seed the local maven repo with the needed dependencies
-curl -L https://github.com/fatdamyr/dev-interview-code/archive/master.zip > master.zip
-unzip master.zip
-cd dev-interview-code-master
+git clone https://github.com/fatdamyr/dev-interview-code.git
+cd dev-interview-code
 mvn clean package
 
 #Clean up the solution code
 cd ..
 rm -rf dev-interview-code
-rm master.zip
-
-#Go back to root
-exit
