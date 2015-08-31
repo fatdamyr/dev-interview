4b8d2549fadfcbcfa23b31a5240ea49e5051c81d
Jason Luck <jason.luck@gmail.com>
2015-08-31 13:07:17 -0400

    Adding JDK8, Maven 3.3.3, KDE Desktop and Eclipse Mars

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -8,6 +8,9 @@ apt-get -y install curl
 
 # You can install anything you need here.
 
+#Install Java
+apt-get -y install openjdk-8-jdk
+
 #Install Ant
 curl http://mirror.cogentco.com/pub/apache//ant/binaries/apache-ant-1.9.6-bin.tar.gz > apache-ant-1.9.6-bin.tar.gz
 tar xvf apache-ant-1.9.6-bin.tar.gz
@@ -16,14 +19,17 @@ rm apache-ant-1.9.6-bin.tar.gz
 ln -s /usr/local/apache-ant/bin/ant /usr/bin/ant
 	
 #Install Maven
-curl http://archive.apache.org/dist/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz > apache-maven-3.2.5-bin.tar.gz
-tar xvf apache-maven-3.2.5-bin.tar.gz
-mv apache-maven-3.2.5 /usr/local/apache-maven
-rm apache-maven-3.2.5-bin.tar.gz
+curl http://archive.apache.org/dist/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz > apache-maven-3.3.3-bin.tar.gz
+tar xvf apache-maven-3.3.3-bin.tar.gz
+mv apache-maven-3.3.3 /usr/local/apache-maven
+rm apache-maven-3.3.3-bin.tar.gz
 ln -s /usr/local/apache-maven/bin/mvn /usr/bin/mvn
 	
 #Install KDE Desktop
-sudo apt-get -y install kde-plasma-desktop
+apt-get -y install kde-plasma-desktop
 
 #Install Eclipse
-
+curl https://eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/R/eclipse-jee-mars-R-linux-gtk.tar.gz&mirror_id=1135 > eclipse.tar.gz
+tar xvf eclipse.tar.gz
+mv eclipse /usr/local/eclipse
+rm eclipse.tar.gz
