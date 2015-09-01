a7898dcdb59096afb26cee48ddf29357d57b147b
Jason Luck <jason.luck@gmail.com>
2015-08-31 20:25:55 -0400

    fixing git repo url

--- scripts/sr-dev-interview.sh
+++ scripts/sr-dev-interview.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #
 
-#Run this as the vagrant user
-su - vagrant
-
 #Add anything here that is needed specifically for the Sr. Java Developer Interview
 
 #Download the POM file and seed the local maven repo with the needed dependencies
-git clone https://github.com/fatdamyr/dev-interview-code.git
-cd dev-interview-code
+git clone https://github.com/fatdamyr/dev-interview-solutions.git
+cd dev-interview-solutions
 mvn clean package
 
 #Clean up the solution code
 cd ..
-rm -rf dev-interview-code
+rm -rf dev-interview-solutions
+
+#Move the maven repository to the 
+mv ~/.m2 ~vagrant/.m2
+chown -R vagrant:vagrant ~vagrant/.m2
